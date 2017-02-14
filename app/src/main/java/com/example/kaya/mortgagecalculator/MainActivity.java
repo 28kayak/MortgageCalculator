566b6f9bcfb855be4c8ae33ff925b40c1dd96910
28kayak <kaya.ota@sjsu.edu>
2017-02-13 19:26:46 -0800

    edit ui & get value of radiogroup

--- app/src/main/java/com/example/kaya/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/kaya/mortgagecalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kaya.mortgagecalculator;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,10 +16,13 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    int intRate;
+    int selectedLoanTerm;
     EditText borrowedAmount;
     TextView payment;
     TextView rateLabel;
-    int intRate;
+
     SeekBar interestRate;
     RadioGroup loanTerm;
     CheckBox tax;
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         tax = (CheckBox) findViewById(R.id.tax);
         insurance = (CheckBox)findViewById(R.id.insurance);
 
+        //set seek bar listener
         interestRate.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
@@ -60,47 +65,23 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
                 }
         );//setOnSeekBarChangeListener()
 
+        //set radio group action listener
 
-    }
+
+
+    }//onCreate
     public void calculate(View v)
     {
-
-        /*borrowedAmount = (EditText)findViewById(R.id.borrowedAmount);
-        payment = (TextView) findViewById(R.id.payment);
-        interestRate = (SeekBar) findViewById(R.id.interestRate);
-        loanTerm = (RadioGroup) findViewById(R.id.loanTerm);//need to check how the value is returned and stored.
-        tax = (CheckBox) findViewById(R.id.tax);
-        insurance = (CheckBox)findViewById(R.id.insurance);
-        */
-
-
-
-        //borrowedAmount.addTextChangedListener(this);
-       /* interestRate.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        payment.setText(interestRate.getProgress());
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-                        //called when touched of seekbar
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-                        //Called when untouched of seekbar
-
-                    }
-                }
-        );*/
+        //should be called when calculate button is clicked
+        int selectedLotnTerm = loanTerm.getCheckedRadioButtonId();
+        RadioButton selected = (RadioButton) findViewById(selectedLotnTerm);
+        payment.setText(selected.getText());
 
        // payment.setText(Integer.valueOf(interestRate.getProgress()));
 
 
 
-    }
+    }//calcualte
     @Override
     public void beforeTextChanged(CharSequence s, int start, int before, int count)
     {}
