f65942421c042c35a30d0b8cfddd87f85dbeceb0
28kayak <kaya.ota@sjsu.edu>
2017-02-14 16:18:54 -0800

    done: case interestRate = 0

--- app/src/main/java/com/example/kaya/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/kaya/mortgagecalculator/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.kaya.mortgagecalculator;
 
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -15,18 +17,22 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity{
 
     int intRate;
+    double monthlyPayment = 0;
     int selectedLoanTerm;
     EditText borrowedAmount;
     TextView payment;
     TextView rateLabel;
-
+    String amount;
     SeekBar interestRate;
     RadioGroup loanTerm;
     CheckBox tax;
     CheckBox insurance;
+    String decimalFormat = "\\d+\\.\\d+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +40,13 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         setContentView(R.layout.activity_main);
 
         borrowedAmount = (EditText)findViewById(R.id.borrowedAmount);
-
         payment = (TextView) findViewById(R.id.payment);
         interestRate = (SeekBar) findViewById(R.id.interestRate);
         rateLabel = (TextView) findViewById(R.id.rateLabel);
         loanTerm = (RadioGroup) findViewById(R.id.loanTerm);//need to check how the value is returned and stored.
         tax = (CheckBox) findViewById(R.id.tax);
         insurance = (CheckBox)findViewById(R.id.insurance);
+
 
         //set seek bar listener
         interestRate.setOnSeekBarChangeListener(
@@ -62,23 +68,69 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
                     public void onStopTrackingTouch(SeekBar seekBar) {
                         //Called when untouched of seekbar
                     }
-                }
+                }//new Seekbar.onSeekBarChangeListener
         );//setOnSeekBarChangeListener()
-
-
-
-
-
     }//onCreate
     public void calculate(View v)
     {
+        monthlyPayment = 0; //init result valaible
+
         //should be called when calculate button is clicked
         int selectedLotnTerm = loanTerm.getCheckedRadioButtonId();
         RadioButton selected = (RadioButton) findViewById(selectedLotnTerm);
-        if(tax.isChecked())
+
+        if(Pattern.matches(decimalFormat, borrowedAmount.getText()) && selectedLotnTerm != -1)
         {
-            payment.setText(tax.getText()+ " is selected");
+            //payment.setText("Amount: " + borrowedAmount.getText())
+            payment.setText("id: " + getLoanTerm(selected.getText()+ ""));
+            int N = getLoanTerm(selected.getText()+ "");
+            double P = Double.parseDouble(borrowedAmount.getText()+"");
+
+            if(intRate == 0)
+            {
+                //interest Rate is 0 %
+                //calculation: monthlyPayment = (borrowedAmount / loanTerms ) + tax
+                monthlyPayment = (P / N);
+                if(tax.isChecked())
+                {
+                    if(insurance.isChecked())
+                    {
+                        //insurance + tax = 2(P*0.001)
+                        monthlyPayment = monthlyPayment + 2*(P * 0.001);
+                    }
+                    else
+                    {
+                        //only tax is checked
+                        monthlyPayment = monthlyPayment + (P * 0.001);
+
+                    }
+
+                }//tax.isChecked()
+                else
+                {
+                    if(insurance.isChecked())
+                    {
+                        //tax is not checked and insurance is checked
+                        monthlyPayment= monthlyPayment + (P * 0.001);
+                    }
+                    else
+                    {
+                        //both are not checked
+                        // no change on monthlyPayment
+                    }
+                }
+                String result = String.format("%1$.3f /m", monthlyPayment);
+                payment.setTextColor(Color.BLUE);
+                payment.setText("Payment: " + result);
+            }
+        }//if(Pattern.matches(decimalFormat, borrowedAmount.getText()))
+        else
+        {
+
+            payment.setText("wrong input");
+            payment.setTextColor(Color.RED);
         }
+
 
 
         //payment.setText(selected.getText());
@@ -88,25 +140,13 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
 
     }//calcualte
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int before, int count)
-    {}
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-    }
-    @Override
-    public void afterTextChanged(Editable s) {
-        String amount = s.toString();
 
-        double decimalAmount;
-        try
-        {
-            decimalAmount = Double.parseDouble(amount);
-            payment.setText(amount);
-        }
-        catch(NumberFormatException nan)
-        {
-            borrowedAmount.setText("Wrong Input!!");
-        }
-    }
-}
+    //helper function to get int type loan term
+    public int getLoanTerm(String text)
+    {
+        int result = 0;
+        String[] temp = text.split(" ");
+        result = Integer.parseInt(temp[0]);
+        return result;
+    }//getLoanTerm
+}//class
